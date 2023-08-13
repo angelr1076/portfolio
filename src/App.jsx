9047f624e8cd23a4924730fc8959bf8982101414
angelr1076 <angelr1076@gmail.com>
2023-08-13 16:05:40 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Welcome!</h1>
+      </header>
+      <main>
+        <section>
+          <h2>
+          </h2>
+        </section>
+      </main>
+      <div className="card">
+      </div>
+    </>
+  )
+}
+
+export default App;
