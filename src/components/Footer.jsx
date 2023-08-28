46888f4a03d208e3139a69f9511a03ce3adf9b89
angelr1076 <angelr1076@gmail.com>
2023-08-28 10:58:15 -0400

    Added font awesome and go icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
+import { FaLinkedin, FaGithub,  } from 'react-icons/fa';
+import { GoMail } from 'react-icons/go';
+
+
 export default function Footer () {
   return (
     <div className="footer">
-      <p>social media icons</p>
-      <p>email</p>
-      <p>phone number</p>
+      <a href='https://www.linkedin.com/in/angelrodriguezlead/' className='footer__link' rel='noreferrer' target='_blank'>
+        <FaLinkedin />
+      </a>
+      <a href='https://github.com/angelr1076' className='footer__link' rel='noreferrer' target='_blank'>
+        <FaGithub />
+      </a>
+      <a href='mailto:angelrod@beachlife.email' className='footer__link' rel='noreferrer' target='_blank'>
+        <GoMail />
+      </a>
     </div>
     )
 }
