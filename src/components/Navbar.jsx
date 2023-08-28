0e8a06b9dbf754aaa032663ae0c0ce44a8be4ace
angelr1076 <angelr1076@gmail.com>
2023-08-27 22:04:19 -0400

    Fixed hamburger expanding on media query and added transition effect along with fixing properties on the react-scroll Link element

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
-import '../assets/styles/style.css';
+import { useState } from 'react';
 import { Link } from 'react-scroll';
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false); 
+
+  const toggleMenu = () => { 
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <nav id='navbar' className='navbar'>
@@ -13,38 +19,39 @@ export default function Navbar() {
           aria-controls='navbarNav'
           aria-expanded='false'
           aria-label='Toggle navigation'
+          onClick={toggleMenu}
         >
           <span className='navbar-toggler-icon'></span>
         </button>
-        <div className='collapse navbar-collapse' id='navbarNav'>
+        <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id='navbarNav'>
           <ul className='navbar-nav'>
             <li className='nav-item'>
               <Link
-                className='nav-link' to='landing' smooth={true} duration={500}>
+                className='nav-link' activeClass="active" to='landing' spy={true} smooth={true} offset={-50} duration={500}>
                 Home
               </Link>
             </li>
             <li className='nav-item'>
               <Link
-                className='nav-link' to='about' smooth={true} duration={500}>
+                className='nav-link' activeClass="active" to='about' spy={true} smooth={true} duration={500}>
                 About
               </Link>
             </li>
             <li className='nav-item'>
               <Link
-                className='nav-link' to='projects' smooth={true} duration={500}>
+                className='nav-link' activeClass="active" to='projects' spy={true} smooth={true} duration={500}>
                 Projects
               </Link>
             </li>
            <li className='nav-item'>
               <Link
-                className='nav-link' to='resume' smooth={true} duration={500}>
+                className='nav-link' activeClass="active" to='resume' spy={true} smooth={true} duration={500}>
               Resume
             </Link>
             </li>
             <li className='nav-item'>
               <Link
-                className='nav-link' to='contact' smooth={true} duration={500}>
+                className='nav-link' activeClass="active" to='contact' spy={true} smooth={true} duration={500}>
               Contact
             </Link>
             </li>
