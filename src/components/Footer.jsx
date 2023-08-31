5986a7f601d3dd034b06e0ad894d668f94aa72f6
angelr1076 <angelr1076@gmail.com>
2023-08-31 06:20:34 -0400

    Added width and height attributes and titles to footer logos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,16 +6,16 @@ import { GoMail } from 'react-icons/go';
 export default function Footer () {
   return (
     <div className="footer">
-      <a href='https://www.linkedin.com/in/angelrodriguezlead/' className='footer__link' rel='noreferrer' target='_blank'>
+      <a href='https://www.linkedin.com/in/angelrodriguezlead/' className='footer__link' rel='noreferrer' target='_blank' title='LinkedIn Profile'>
         <FaLinkedin />
       </a>
-      <a href='https://github.com/angelr1076' className='footer__link' rel='noreferrer' target='_blank'>
+      <a href='https://github.com/angelr1076' className='footer__link' rel='noreferrer' target='_blank' title='Github Profile'>
         <FaGithub />
       </a>
-      <a href='mailto:devlife@beachlife.email' className='footer__link' rel='noreferrer' target='_blank'>
+      <a href='mailto:devlife@beachlife.email' className='footer__link' rel='noreferrer' target='_blank' title='Send Me An Email'>
         <GoMail />
       </a>
-      <p>Built With <FaReact /> <SiVite /></p>
+      <p>Built With <FaReact className="react-logo" alt="React logo" title="This project was built using React" /> <SiVite className="vite-logo" title='Vite build tool was used' /></p>
     </div>
     )
 }
