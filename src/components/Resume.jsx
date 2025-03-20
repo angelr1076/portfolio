f149c91145785a3b412e8043375e88f78a23ffff
angelr1076 <angelr1076@gmail.com>
2025-03-20 15:56:02 -0400

    - Added new version of resume along with the url directing to the github resume repository

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -9,7 +9,7 @@ export default function Skills() {
       <h2 className={`page-header page-header-${theme}`}>Experience</h2>
       <div className='resume__container'>
         <a
-          href='https://www.cake.me/angel-rod'
+          href='https://github.com/angelr1076/Resume/blob/main/rodriguez_angel_resume_2025-03-25.pdf'
           className='resume__link'
           rel='noreferrer'
           target='_blank'>
