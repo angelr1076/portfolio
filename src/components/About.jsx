6ecb6f17b71ab4a1c24134d2576b681ca21cafe8
angelr1076 <angelr1076@gmail.com>
2025-07-10 14:12:39 -0400

    Updated resume in the public folder and updated resume job description wording for current and previous positions

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,25 +13,36 @@ export default function About() {
         </div>
         <div className='bio-desc'>
           <p>
-            Driven by a childhood curiosity to understand how things work, I’ve
-            always been passionate about deconstructing and reassembling the
-            world around me.
+            Application Developer with 6+ years of experience building
+            data-driven internal tools, mapping systems, and workflow automation
+            that drive operational efficiency and strategic decision-making.
+            Proven track record of:
           </p>
-
+          <ul>
+            <li>
+              Reducing costs by automating business processes, such as saving
+              $90K in file storage upgrades through streamlined attachment
+              migrations.
+            </li>
+            <li>
+              Enabling nationwide strategic sales targeting with interactive
+              mapping platforms integrating HereMaps APIs and JavaScript.
+            </li>
+            <li>
+              Improving workflows for 90% of employees by developing real-time
+              distance calculation tools with Google Maps APIs, optimizing
+              logistical planning.
+            </li>
+            <li>
+              Enhancing client data management and clinical workflows with API
+              integrations.
+            </li>
+          </ul>
           <p>
-            In my current role, I led an attachment storage migration from one
-            platform to another, helping the company avoid a near six-figure
-            increase in infrastructure costs. I also built mapping tools to
-            streamline logistics operations and developed internal applications
-            that significantly reduced support overhead. I focus on designing
-            systems that are intuitive, dependable, and allow people to focus on
-            their work, not get bogged down by broken processes.
-          </p>
-
-          <p>
-            I’m committed to sharpening my skills, staying curious, and
-            continuing to grow as a lifelong learner and developer who builds
-            tools that make a difference.
+            Skilled at partnering with cross-functional teams to build scalable,
+            maintainable, and impactful applications. Adept in React,
+            JavaScript, and API development with a strong focus on performance,
+            accessibility, and user-centered design.
           </p>
         </div>
       </div>
